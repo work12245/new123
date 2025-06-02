aa2274ee2122cdfcb4486ec5c6c04bd7b89c9e94
ashish <ashishverma320664@gmail.com>
2025-06-02 13:58:27 +0530

    first commit

---
+++ public/user/js/include.js
@@ -0,0 +1,14 @@
+// includes.js
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+      .then((res) => res.text())
+      .then((data) => {
+        document.getElementById("header").innerHTML = data;
+      });
+  
+    fetch("footer.html")
+      .then((res) => res.text())
+      .then((data) => {
+        document.getElementById("footer").innerHTML = data;
+      });
+  });
